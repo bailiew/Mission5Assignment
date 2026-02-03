c6f2b2b817a330867009a6c81ac28222bcff65e7
Bailie Whetten <bailiewhetten@gmail.com>
2026-02-02 21:17:27 -0700

    third commit

--- Mission5Assignment/obj/Debug/net10.0/Mission5Assignment.AssemblyInfo.cs
+++ Mission5Assignment/obj/Debug/net10.0/Mission5Assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission5Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b2b9c099f4fe301bb6ce119f3a3b0183086d13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission5Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission5Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Mission5Assignment/wwwroot/js/site.js
+++ Mission5Assignment/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function() {
+    $("#btnSubmit").on("click", function () {
+        const hours = parseFloat($("#txtHours").val());
+        const rate = parseFloat($("#txtRate").data("rate"));
+
+        // if the user does not enter hours 
+        if (isNaN(hours) || hours < 0) {
+            $("#txtTotal").val("Enter valid hours");
+            return
+        }
+
+        const total = hours * rate;
+
+        // Format as currency
+        $("#txtTotal").val(total.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD"
+        }));
+
+    });
+
+});
